00c6badb907980dc07bd3267b344e3654b7a0424
Tutos Rive <108991712+tutosrive@users.noreply.github.com>
2025-09-19 19:20:34 -0500

    Update controllers, add something methods to can jump, move and others

--- src/app/controllers/Road.controller.js
+++ src/app/controllers/Road.controller.js
@@ -1,15 +1,25 @@
 import RoadGame from '../components/game/road.game.mjs';
+import OwnUtils from '../utils/own/own.mjs';
 
 export default class RoadController {
+    #roadDiv;
     async init() {
         this.containerRoad = document.querySelector('#road-game');
-        this.roadDiv = null;
         this.roadHtml = await RoadGame();
 
         this.#innerRoad();
+        this.#roadDiv = document.querySelector('#road-container');
     }
 
     #innerRoad() {
         this.containerRoad.innerHTML = this.roadHtml;
+    }
+
+    getHeight() {
+        console.log(this.#roadDiv);
+        const roadDivStyles = getComputedStyle(this.#roadDiv);
+
+        const heightRoad = OwnUtils.fromPixelsToNumber(roadDivStyles.height);
+        return heightRoad;
     }
 }
