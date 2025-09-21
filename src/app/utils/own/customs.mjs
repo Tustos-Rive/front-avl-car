486f109d72efbdd7bec2e6ecb7903f92d125c591
Tutos Rive <108991712+tutosrive@users.noreply.github.com>
2025-09-20 21:20:55 -0500

    Update customs translations (only one)

--- src/app/utils/own/customs.mjs
+++ src/app/utils/own/customs.mjs
@@ -3,9 +3,10 @@
  * se presiona el botón "Añadir" del "Dialog"
  * @param {String} span Mensaje de alerta
  */
-export function toastBeforeAddRecord(msg = null) {
-  const span = msg ?? 'Debe <span class="text-warning">completar</span> el formulario'
-  Toast.show({ message: `${span} antes de presionar el botón <button class="btn btn-primary">${addButton}</button>`, mode: 'warning', duration: 1000 })
+export function toastBeforeAddRecord(msg = null, button = null) {
+    const span = msg ?? 'Should <span class="text-warning">fill</span> the form';
+    const btn = button ?? addButton;
+    Toast.show({ message: `${span} before press the button <button class="btn btn-primary">${btn}</button>`, mode: 'warning', duration: 1000 });
 }
 
 /**
@@ -13,24 +14,24 @@ export function toastBeforeAddRecord(msg = null) {
  * @param {String} msg Opción actual. `Ej: clientes`
  */
 export function showInfoAboutUse(msg) {
-  let cont // Para regular las veces que se mostrará el "Toast"
-  const countOnLocalStorage = localStorage.getItem(`alertInfoOn${msg}`)
-  // Verificar la existencia del contador
-  if (!countOnLocalStorage) {
-    // Si no existe, crearlo
-    localStorage.setItem(`alertInfoOn${msg}`, 1)
-    cont = countOnLocalStorage
-  } else {
-    // Castear el valor del contador almacenado
-    cont = parseInt(countOnLocalStorage)
-    // Contador menor a 3? seguir aumentando contador : null
-    cont < 2 ? localStorage.setItem(`alertInfoOn${msg}`, cont + 1) : null
-  }
-  // Si el contador es menor a 2, el "Toast" se mostrará
-  cont < 2 ? Toast.show({ message: `Puede <span class="text-info">agregar</span> ${msg} con <span class="d-inline">${addRowButton}</span> y <span class="text-danger">eliminarlos</span> con ${deleteRowButton()}` }) : null
+    let cont; // Para regular las veces que se mostrará el "Toast"
+    const countOnLocalStorage = localStorage.getItem(`alertInfoOn${msg}`);
+    // Verificar la existencia del contador
+    if (!countOnLocalStorage) {
+        // Si no existe, crearlo
+        localStorage.setItem(`alertInfoOn${msg}`, 1);
+        cont = countOnLocalStorage;
+    } else {
+        // Castear el valor del contador almacenado
+        cont = parseInt(countOnLocalStorage);
+        // Contador menor a 3? seguir aumentando contador : null
+        cont < 2 ? localStorage.setItem(`alertInfoOn${msg}`, cont + 1) : null;
+    }
+    // Si el contador es menor a 2, el "Toast" se mostrará
+    cont < 2 ? Toast.show({ message: `Puede <span class="text-info">agregar</span> ${msg} con <span class="d-inline">${addRowButton}</span> y <span class="text-danger">eliminarlos</span> con ${deleteRowButton()}` }) : null;
 }
 
-export const classesModal = 'position-absolute top-50 start-50 translate-middle bg-dark col-12 col-sm-10 col-md-9 col-lg-8 col-xl-7'
+export const classesModal = 'position-absolute top-50 start-50 translate-middle bg-dark col-12 col-sm-10 col-md-9 col-lg-8 col-xl-7';
 
 /**
  * Crear popover
@@ -41,21 +42,21 @@ export const classesModal = 'position-absolute top-50 start-50 translate-middle 
  * @returns
  */
 export function popover(title = '', message = '', buttons = [], classPop = '') {
-  const body = document.querySelector('body')
-  let buttonsPop = ''
-  const pop = document.createElement('div')
-  const classes = classPop.split(' ')
+    const body = document.querySelector('body');
+    let buttonsPop = '';
+    const pop = document.createElement('div');
+    const classes = classPop.split(' ');
 
-  body.querySelector('#pop-pup') ? body.querySelector('#pop-pup').remove() : null
-  pop.setAttribute('popover', 'manual')
-  pop.classList.add(...classes)
-  pop.id = `pop-pup-${Helpers.idRandom()}`
+    body.querySelector('#pop-pup') ? body.querySelector('#pop-pup').remove() : null;
+    pop.setAttribute('popover', 'manual');
+    pop.classList.add(...classes);
+    pop.id = `pop-pup-${Helpers.idRandom()}`;
 
-  buttons.forEach(btn => {
-    buttonsPop += `<div class="col">${btn}</div>`
-  })
+    buttons.forEach((btn) => {
+        buttonsPop += `<div class="col">${btn}</div>`;
+    });
 
-  pop.innerHTML = `
+    pop.innerHTML = `
     <div class="p-4" style="max-width: 450px;">
       <div class="pop-header">
         <h5 class="pop-title">${title}</h5>
@@ -71,9 +72,9 @@ export function popover(title = '', message = '', buttons = [], classPop = '') {
         </div>
       </div>
     </div>
-  `
-  body.insertAdjacentElement('beforeend', pop)
-  return pop
+  `;
+    body.insertAdjacentElement('beforeend', pop);
+    return pop;
 }
 
 /**
@@ -81,14 +82,14 @@ export function popover(title = '', message = '', buttons = [], classPop = '') {
  * @param {*} pop Elemento HTML con atributo **popover**
  */
 export function showPopover(pop) {
-  pop.showPopover()
-  document.querySelectorAll('body > *').forEach(e => {
-    e.addEventListener('click', ev => {
-      ev.preventDefault()
-      ev.stopPropagation()
-    })
-  })
-  document.querySelector('body').style.filter = 'blur(2px)'
+    pop.showPopover();
+    document.querySelectorAll('body > *').forEach((e) => {
+        e.addEventListener('click', (ev) => {
+            ev.preventDefault();
+            ev.stopPropagation();
+        });
+    });
+    document.querySelector('body').style.filter = 'blur(2px)';
 }
 
 /**
@@ -96,7 +97,7 @@ export function showPopover(pop) {
  * @param {*} pop Elemento HTML con atributo **popover**
  */
 export function closePopover(pop) {
-  // pop.hidePopover()
-  pop.remove()
-  document.querySelector('body').style.filter = ''
+    // pop.hidePopover()
+    pop.remove();
+    document.querySelector('body').style.filter = '';
 }
